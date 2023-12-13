5d72e9067a0cfdeb0868cbf27739a756781ca60a
Camila Alani <alanicamila124@gmail.com>
2023-12-13 01:24:01 -0300

    agregamos un props en el componente Informacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Informacion from "./components/Informacion";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
+  const anioActual = 2023;
+
   return (
-  <section>
+  <section className="container">
     <h1 className="display-3 text-center">Proyecto de React</h1>
-    <Informacion></Informacion>
-    <Informacion/>
+    {/* <Informacion></Informacion> */}
+    <Informacion comision="c76i" anioActualProps={anioActual}/>
   </section>
   );
 }
